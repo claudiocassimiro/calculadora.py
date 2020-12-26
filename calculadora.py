4a0d89aff43e7f3fd504ad3073194807b4e64e1f
Day's Surfing <days_surfing@live.com>
2020-12-26 07:05:01 -0300

    Nova versão da calculadora,py, conta agora com operações de subtração, divisão e multiplicação, também é possivel operações com até 4 valores.

--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,74 @@
-print("{} calculadora_soma.py {}".format("="*12, "="*12))
-num1 = int(input("Digite um valor: "))
-num2 = int(input("Digite outro valor: "))
-outro_valor = str(input("Deseja digitar mais um valor? [S/N]: " ).upper())
+print("{}Calculadora.py{}".format("=" * 6, "=" * 6))
+
+num1 = float(input("Digite um valor: "))
+
+num2 = float(input("Digite outro valor: "))
+
+outro_valor = str(input("Deseja digitar outro valor? [S/N]: ").upper())
+
 if outro_valor == "S":
-    num3 = int(input("Digite outro valor: "))
-    outro_valor2 = str(input("Deseja digitar mais um valor? [S/N]: " ).upper())
-    if outro_valor2 == "S":
-        num4 = int(input("Digite outro valor: "))
-        print("O resultado da soma entre os quatros valores é igual a {}.".format(num1 + num2 + num3 + num4))
-    else:
-        print("O resultado da soma entre os três valores é igual a {}.".format(num1 + num2 + num3))
-if outro_valor != "S":
-    print("O resultado da soma entre os dois valores é igual a {}.".format(num1 + num2))
-print("{} calculadora_soma.py {}".format("="*12, "="*12))
+    num3 = float(input("Digite o terceiro valor: "))
+
+else:
+    print("Digite [1] para Soma")
+    print("Digite [2] Subtração")
+    print("Digite [3] Divisão")
+    print("Digite [4] Multiplicação")
+    operacao = int(input("Qual a operação desejada?: "))
+
+    if operacao == 1:
+        print(num1 + num2)
+        
+
+    if operacao == 2:
+        print(num1 - num2) 
+
+    if operacao == 3:
+        print(num1 / num2) 
+
+    if operacao == 4:
+        print(num1 * num2)
+
+outro_valor2 = str(input("Deseja digitar outro valor? [S/N]: ").upper())
+
+if outro_valor2 == "S":
+    num4 = float(input("Digite o quarto valor: "))
+
+    print("Digite [1] para Soma")
+    print("Digite [2] Subtração")
+    print("Digite [3] Divisão")
+    print("Digite [4] Multiplicação")
+    operacao = int(input("Qual a operação desejada?: "))
+
+    if operacao == 1:
+        print(num1 + num2 + num3 + num4)
+
+    if operacao == 2:
+        print(num1 - num2 - num3 - num4)
+
+    if operacao == 3:
+        print(num1 / num2 / num3 / num4)
+
+    if operacao == 4:
+        print(num1 * num2 * num3 * num4)
+
+else:
+    print("Digite [1] para Soma")
+    print("Digite [2] Subtração")
+    print("Digite [3] Divisão")
+    print("Digite [4] Multiplicação")
+    operacao = int(input("Qual a operação desejada?: "))
+
+        if operacao == 1:
+            print(num1 + num2 + num3)
+
+        if operacao == 2:
+            print(num1 - num2 - num3)
+
+        if operacao == 3:
+            print(num1 / num2 / num3)
+
+        if operacao == 4:
+            print(num1 * num2 * num3)
+
+print("{}Calculadora.py{}".format("=" * 6, "=" * 6))
